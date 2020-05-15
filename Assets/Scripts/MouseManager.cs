f8ebe90dfd9331a9b8bfdfbef4c411560db3a007
6kornive <6kornive@informatik.uni-hamburg.de>
2020-05-15 18:43:54 +0200

    MouseManager Script
    First Assignment Part1

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class MouseManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public float speed = 100.0f;
+    // there are limits for the minimum and maximum zoom
+    public float minFov = 15.0f;
+ 	public float maxFov = 90.0f;
+ 	public float sensitivity = 10.0f;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        // while holding the right mouse button, moving the mouse pans the camera (on the XZ-plane)
+        if (Input.GetMouseButton(1)) {
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            if (Input.GetAxis("Mouse X") != 0)
+            {
+                Vector3 move = new Vector3(Input.GetAxisRaw("Mouse X") * speed * Time.deltaTime, 0, 0);
+                transform.Translate(move, Space.World);
+            }
+            if (Input.GetAxis("Mouse Y") != 0)
+            {
+                Vector3 move = new Vector3(0, 0, Input.GetAxisRaw("Mouse Y") * speed * Time.deltaTime);
+                transform.Translate(move, Space.World);
+            }
+        }
+        // scrolling the mouse wheel zooms the camera in and out
+        var fov = Camera.main.fieldOfView;
+   		fov += Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+   		fov = Mathf.Clamp(fov, minFov, maxFov);
+   		Camera.main.fieldOfView = fov;
         
     }
 }
