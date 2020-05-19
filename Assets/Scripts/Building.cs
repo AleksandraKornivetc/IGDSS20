693b9b7b090498e93e966178d0a017a01e5f952c
viridian44 <33371218+viridian44@users.noreply.github.com>
2020-05-19 18:05:47 +0200

    building script and economy, first prefab
    finished building script
    finished economy system (money only)
    created first building prefab

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Building : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    #region Attributes
+    public string _name;
+    public BuildingType _buildingType;
+    public int _upkeep;
+    public int _constructionCostMoney;
+    public int _constructionCostPlanks;
+    public Tile _tile;
 
-    }
+    float _efficiencyValue; // Calculated, so not public
+    public int _generationInterval;
+    public int _outputCount;
+    public Tile.TileTypes[] _possibleTiles;
+    public Tile.TileTypes _scalingNeighborTile; // Does not scale if null
+    [Range(0, 6)]
+    public int _minScalingNeighborTiles;
+    [Range(0, 6)]
+    public int _maxScalingNeighborTiles;
+    public GameManager.ResourceTypes[] _inputResources;
+    public GameManager.ResourceTypes _outputResource;
+    #endregion
 
-    // Update is called once per frame
-    void Update()
-    {
 
-    }
+    #region Enumerations
+    public enum BuildingType { Empty, Fishery, Lumberjack, Sawmill, SheepFarm, FrameworkKnitters, PotatoFarm, SchnappsDistillery };
+    #endregion
+
 }
